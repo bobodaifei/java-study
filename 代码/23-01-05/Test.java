75bc65fbe0a8c3aff305d6ff35c0ae732a63ada0
bobodaifei <2194296863@qq.com>
2023-01-06 14:05:23 +0800

    14.05

--- 代码/23-01-05/Test.java
+++ 代码/23-01-05/Test.java
@@ -1,8 +1,7 @@
 //test 完成1+1
-
 public class Test {
   public static void main(String[] args) {
-    int res = 1 + 1;
+    int res = 1 + 2;
     System.out.println("结果=" + res);
   }
 }
---
+++ 代码/23-01-06/Hello.java
@@ -0,0 +1,13 @@
+//java快速入门
+
+//1.public class Hello 表示Hello是一个类，public公有的类
+public class Hello{
+
+  //2.主方法，程序入口
+  public static void main(String[] args){
+    //3.输出到屏幕上
+    System.out.println("hellw,world");
+
+  }
+
+}

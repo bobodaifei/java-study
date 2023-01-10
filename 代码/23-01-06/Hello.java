e5c9905e719b36e6812a9cad828fd07e18b7bb76
bobodaifei <2194296863@qq.com>
2023-01-10 13:40:17 +0800

    01-10提交

--- 代码/23-01-06/Hello.java
+++ 代码/23-01-06/Hello.java
@@ -6,8 +6,33 @@ public class Hello{
   //2.主方法，程序入口
   public static void main(String[] args){
     //3.输出到屏幕上
-    System.out.println("hellw,world");
+    System.out.println("hello,徐纪波 is study! ");
 
   }
 
+}
+
+//编译后，每个class类都会生成一个.class文件
+class Dog{
+  public static void main(String[] args){
+    System.out.println("Dog is study!");
+  }
+}
+class Cat {
+  public static void main(String[] args) {
+    System.out.println("Cat is study!");
+  }
+}
+
+//转义符
+class ChangeChar {
+  public static void main(String[] args) {
+    System.out.println("这是一个\t字符,实现\t对齐");
+    System.out.println("这是一个\n字符");
+    System.out.println("这是一个\\字符");
+    System.out.println("这是一个\'字符");
+    System.out.println("这是一个\"字符");
+    System.out.println("这是一个\r字符");
+    System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+  }
 }

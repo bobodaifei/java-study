96fa46a1bb41daa30514eccc4c495b07ce26b04f
bobodaifei <2194296863@qq.com>
2023-01-10 17:13:00 +0800

    01-10

--- myCode/java_23_01_10/Comment.java
+++ myCode/java_23_01_10/Comment.java
@@ -1,1 +1,10 @@
 package java_study.myCode.java_23_01_10;
+
+public class Comment {
+   //2.主方法，程序入口
+  public static void main(String[] args){
+    //3.输出到屏幕上
+    System.out.println("hello,徐纪波 is study! ");
+
+  }
+}

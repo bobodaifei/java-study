9458c99b1065c9a3004b90dac41aef0ebb9d7178
bobodaifei <2194296863@qq.com>
2023-01-30 13:31:46 +0800

    123

---
+++ myCode/java_23_01_10/Comment01.java
@@ -0,0 +1,15 @@
+package java_study.myCode.java_23_01_10;
+
+/**
+ * @author 波波
+ * @version 1.0
+ */
+
+public class Comment01 {
+  // 2.主方法，程序入口
+  public static void main(String[] args) {
+    // 3.输出到屏幕上
+    System.out.println("hello XJB is study! ");
+
+  }
+}

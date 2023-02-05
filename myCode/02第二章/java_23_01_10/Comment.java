479c22ef025085d3d31d32b0ca04b0c9ec00e65e
bobodaifei <2194296863@qq.com>
2023-02-05 13:59:37 +0800

    2-5

--- myCode/02第二章/java_23_01_10/Comment.java
+++ myCode/02第二章/java_23_01_10/Comment.java
@@ -1,4 +1,4 @@
-package java_study.myCode.java_23_01_10;
+
 
 public class Comment {
    //2.主方法，程序入口
---
+++ myCode/03第三章/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+  public static void main(String[] args) {
+    int a = 1;
+    int b = 2;
+    b = 89;
+    System.out.println("结果a=" + a);
+    System.out.println("结果b=" + b);
+  }
+}

430905f989f858edf1a93bf3d4840eae9858ee24
bobodaifei <2194296863@qq.com>
2023-07-21 17:27:37 +0800

    111

--- springboot/purview/src/main/java/com/bobo/purview/PurviewApplication.java
+++ springboot/purview/src/main/java/com/bobo/purview/PurviewApplication.java
@@ -1,15 +1,27 @@
 package com.bobo.purview;
 
+import com.bobo.purview.entity.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 @MapperScan("com.bobo.purview.mapper")
 public class PurviewApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(PurviewApplication.class, args);
+    ConfigurableApplicationContext run = SpringApplication.run(PurviewApplication.class, args);
+    RedisTemplate<String, User> redisTemplate = run.getBean("redisTemplate", RedisTemplate.class);
+    User user = new User();
+    user.setUserId(1);
+    redisTemplate.opsForValue().set("token", user);
+
+    RedisTemplate<String, String> redisTemplate1 = run.getBean("redisTemplate", RedisTemplate.class);
+    System.out.println(redisTemplate1.opsForValue().get("token"));
+
+
   }
 
 }

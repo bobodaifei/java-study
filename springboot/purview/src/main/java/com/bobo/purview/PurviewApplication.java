219dac951d5d555840b7ec0a68070756b98bc139
bobodaifei <2194296863@qq.com>
2023-09-11 10:05:21 +0800

    111

--- springboot/purview/src/main/java/com/bobo/purview/PurviewApplication.java
+++ springboot/purview/src/main/java/com/bobo/purview/PurviewApplication.java
@@ -1,8 +1,17 @@
 package com.bobo.purview;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Handler;
 
 @SpringBootApplication
 @MapperScan("com.bobo.purview.mapper")
@@ -14,4 +23,10 @@ public class PurviewApplication {
 
 
 
+}
+
+class AA{
+  public static void main(String[] args) {
+
+  }
 }
--- springboot/purview/src/main/java/com/bobo/purview/component/config/WebConfigurer.java
+++ springboot/purview/src/main/java/com/bobo/purview/component/config/WebConfigurer.java
@@ -4,8 +4,14 @@ import com.bobo.purview.interceptors.PermissionInterceptor;
 import com.bobo.purview.interceptors.TokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
@@ -25,6 +31,7 @@ public class WebConfigurer implements WebMvcConfigurer {
     registry.addInterceptor(permissionInterceptor)
             .addPathPatterns("/**")
             .excludePathPatterns("/user/login");
+
   }
 
 }
